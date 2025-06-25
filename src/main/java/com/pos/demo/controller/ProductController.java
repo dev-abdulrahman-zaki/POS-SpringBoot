0dcc5463f4e9a4a41c75c7eedc46d38aaeeb19d4
dev-abdulrahman-zaki <dev-abdulrahman-zaki@gmail.com>
2025-06-25 14:21:42 +0300

    feat(product): add product creation endpoint and update db config
    - Add POST endpoint for creating products in ProductController
    - Implement product creation logic in ProductDao
    - Update application.properties with MySQL database configuration
    - Update .gitignore to exclude target directory

--- src/main/java/com/pos/demo/controller/ProductController.java
+++ src/main/java/com/pos/demo/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.pos.demo.controller;
 
 import com.pos.demo.dao.ProductDao;
+import com.pos.demo.entity.ProductEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductDao productDao;
+
+    @PostMapping("add-product")
+    public ProductEntity addProduct(@RequestBody ProductEntity product){
+        return this.productDao.addProduct(product);
+    }
 
 }
--- src/main/java/com/pos/demo/dao/ProductDao.java
+++ src/main/java/com/pos/demo/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.pos.demo.dao;
 
+import com.pos.demo.entity.ProductEntity;
 import com.pos.demo.repository.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProductDao {
     private final ProductRepo productRepo;
+    public ProductEntity addProduct(ProductEntity product){
+        return this.productRepo.save(product);
+    }
 }
