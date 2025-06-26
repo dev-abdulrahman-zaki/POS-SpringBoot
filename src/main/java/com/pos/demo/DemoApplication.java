1a5c013d3f440c30901d249ed39dc605164a036a
dev-abdulrahman-zaki <dev-abdulrahman-zaki@gmail.com>
2025-06-26 10:41:32 +0300

    refactor(build): migrate from maven to gradle build system
    - Remove maven-related files and configurations
    - Add gradle wrapper and configuration files
    - Update .gitignore and .gitattributes for gradle
    - Simplify application.properties and remove unused entities/repositories
    - Add new DemoApplication class as entry point

--- src/main/java/com/pos/demo/DemoApplication.java
+++ src/main/java/com/pos/demo/DemoApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class POSApplication {
+public class DemoApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(POSApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
 }
